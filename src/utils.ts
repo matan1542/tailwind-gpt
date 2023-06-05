8b4ebb4276e3b1646f582222b79a6dc9000d213a
matan1542 <matan8380@gmail.com>
2023-06-05 11:15:19 +0300

    readme

--- src/utils.ts
+++ src/utils.ts
@@ -3,38 +3,40 @@ import { EXTENSION_NAME } from "./constants";
 import { displayGPTAnswerFormated } from "./extension";
 
 export function getConfigName(name: string) {
-  return `${EXTENSION_NAME}.${name}`;
+	return `${EXTENSION_NAME}.${name}`;
 }
 
 export function handleStream() {
-  let resText = ''
-  return ({
-    stream: (value: BufferSource, decoder: TextDecoder, panel: WebviewPanel) => {
-      const chunk = decoder.decode(value)
-      const lines: string[] = chunk.split("\n");
-      const parsedLines = lines
-        .map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
-        .filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
-        .map((line) => JSON.parse(line)); // Parse the JSON string
+	let resText = ''
+	return ({
+		stream: (value: BufferSource, decoder: TextDecoder, panel: WebviewPanel) => {
+			const chunk = decoder.decode(value)
+			const lines: string[] = chunk.split("\n");
+			const parsedLines = lines
+				.map((line) => line.replace(/^data: /, "").trim()) // Remove the "data: " prefix
+				.filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
+				.map((line) => JSON.parse(line)); // Parse the JSON string
 
-      for (const parsedLine of parsedLines) {
-        const { choices } = parsedLine;
-        const { delta } = choices[0];
-        const { content } = delta;
-        // Accumulate the content
-        if (content) {
-          resText += content
-          displayGPTAnswerFormated(resText, panel)
-        }
-      }
-    }
-  })
+			for (const parsedLine of parsedLines) {
+				const { choices } = parsedLine;
+				const { delta } = choices[0];
+				const { content } = delta;
+				// Accumulate the content
+				if (content) {
+					resText += content
+					displayGPTAnswerFormated(resText, panel)
+				}
+			}
+		}
+	})
 }
 
 export function createPrompt(classList: string[]) {
-  return `
+	return `
 					The following is a classlist. Some of the classes in the class list might be 
 					a Tailwind CSS classes. Please provide explanations for each class.
+					For color classes such as tahiti that equals to '#3ab7bf' please be implicit,
+					and return in hexadecimal
 					For example, given the following class list:
 					"absolute grid pt-2"
 					Please return the following output:
